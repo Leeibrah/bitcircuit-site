b998d7b8f7b059e6ee612186d220272701d72ee0
magigedaniel <magigedaniel@gmail.com>
2015-09-30 17:13:12 +0300

    Mail

---
+++ mail.php
@@ -0,0 +1,49 @@
+<?php
+
+$senderName='Sender Name';
+$senderEmail='magige@mephatech.com';
+$replyToEmail='sasa@sasa.com';
+$replyToName='New User';
+
+$receiverName='Receiver Name';
+$receiverEmail='magigedaniel@gmail.com';
+
+$mailSubject='Subject';
+$mailBody='Message body from a contact';
+
+
+require 'PHPMailer/PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'ns5.hostyetu.com;ns6.hostyetu.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'mephatec';                 // SMTP username
+$mail->Password = '2014hcetahpem';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+$mail->setFrom($senderEmail, 'BitCircuit Contacted');
+$mail->addAddress($receiverEmail, $receiverName);     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+$mail->addReplyTo($replyToEmail, $replyToName);
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = $mailSubject;
+$mail->Body    = $mailBody;
+//$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}

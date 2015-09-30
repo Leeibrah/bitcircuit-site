d31bcbc01247ca0f6446b632890ce1e3d688d267
magigedaniel <magigedaniel@gmail.com>
2015-09-30 14:26:55 +0300

    new latest

--- PHPMailer/examples/gmail.php
+++ PHPMailer/examples/gmail.php
@@ -1,7 +1,6 @@
 <?php
 /**
- * This example shows settings to use when sending via Google's Gmail servers.
- */
+ * This example shows settings to use when sending via Google's Gmail servers.*/
 
 //SMTP needs accurate times, and the PHP time zone MUST be set
 //This should be done in your php.ini, but this is how to do it if you don't have access to that

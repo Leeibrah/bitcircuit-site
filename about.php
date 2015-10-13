4b1802e849fdaeeecced878767308ee4d03b2bbb
Magige Daniel <magigedaniel@gmail.com>
2015-10-13 12:59:45 +0300

    Sawa

--- about.php
+++ about.php
@@ -10,140 +10,172 @@ require_once('header.php');
 <?php
 require_once('menu.php');
 ?>
-    <!-- HEADER END -->
+<!-- HEADER END -->
 
-    <!-- **************************************
-                    PAGE TITLE IMAGE
-    *************************************** -->
-    <section class="do-parallax-page-title-section" data-stellar-background-ratio="0.5" data-stellar-vertical-offset="110">
-        <!-- Parallax Overlay -->
-        <div class="do-dark-overlay"></div>
+<!-- **************************************
+                PAGE TITLE IMAGE
+*************************************** -->
+<section class="do-parallax-page-title-section" data-stellar-background-ratio="0.5" data-stellar-vertical-offset="110">
+    <!-- Parallax Overlay -->
+    <div class="do-dark-overlay"></div>
 
-        <div class="container">
-            <div class="row">
-                <!-- Page Title -->
-                <div class="do-page-title">
-                    <h1>ABOUT US</h1>
-                    <p>BEST THEME FOR BUSINESS</p>
+    <div class="container">
+        <div class="row">
+            <!-- Page Title -->
+            <div class="do-page-title">
+                <h1>ABOUT US</h1>
+
+                <p>...BEST IT PRODUCT...</p>
+            </div>
+            <!-- Page Title End -->
+        </div>
+    </div>
+</section>
+<!-- do Main Slider End -->
+
+<!--================================
+    ABOUT US DETAILS SECTION
+=================================-->
+<section class="do-about-us-section">
+    <div class="container">
+        <div class="row">
+            <!-- SECTION SUBHEADING -->
+            <h2 class="do-section-subheading col-md-12 col-sm-12 col-xs-12">Why we are expert</h2>
+            <!-- SECTION SUBHEADING END -->
+
+            <div class="do-about-us-details">
+                <!-- TEXT ROW 1 -->
+                <div class="col-md-4 col-sm-4 col-xs-12 do-about-text-row">
+                    <p>
+                        BitCircuit Technologies is an indigenous Information and Communication Technology (ICT)
+                        company established to carry out the latest Information Communication Technology at its best in
+                        Africa.
+                    </p>
+
+                    <P>
+                        BitCircuit is a company that brings together innovation and specialization to develop
+                        Information and Technology products for NGOs, Governmnet, Small and Medium Enterprises. For the
+                        past two years the company has provided innovative solutions to a number of enterprises across
+                        Africa.
+                    </p>
                 </div>
-                <!-- Page Title End -->
+                <!-- TEXT ROW 1 END -->
+
+                <!-- TEXT ROW 2 -->
+                <div class="col-md-4 col-sm-4 col-xs-12 do-about-text-row">
+                    <p>
+                        BitCircuit has positioned itself as the leader in identifying, developing, implementing
+                        and maintaining dynamic website development concepts through the utilization of
+                        multiple protocols, latest software and media with a
+                        definitive focus on Internet based communication for instance <a href="http://teamsync.co"
+                                                                                         style="color: red;"
+                                                                                         target="_blank">Teamsync</a>.
+                        that is a web portal that has made it easy for a number of organization to run their operation
+                        effectively
+                    </p>
+                    <p>
+                        We are also offering the best IT solution in the current competitive market
+                    </p>
+                </div>
+                <!-- TEXT ROW 2 END -->
+
+                <!-- SKILLBAR -->
+                <div class="col-md-4 col-sm-4 col-xs-12 do-our-skill">
+
+                    <!-- Design Skill Bar -->
+                    <div class="do-skillbar clearfix" data-percent="70">
+                        <p class="do-skillbar-title">Design<span class="skill-bar-percent"></span></p>
+
+                        <div class="do-skillbar-bar-wrapper">
+                            <div class="do-skillbar-bar"></div>
+                        </div>
+                    </div>
+                    <!-- Web Skill Bar End -->
+                    <div class="do-skillbar clearfix" data-percent="80">
+                        <p class="do-skillbar-title">Web Technology<span class="skill-bar-percent"></span></p>
+
+                        <div class="do-skillbar-bar-wrapper">
+                            <div class="do-skillbar-bar"></div>
+                        </div>
+                    </div>
+                    <!-- Training Skill Bar End -->
+
+
+                    <div class="do-skillbar clearfix" data-percent="80">
+                        <p class="do-skillbar-title">Training<span class="skill-bar-percent"></span></p>
+
+                        <div class="do-skillbar-bar-wrapper">
+                            <div class="do-skillbar-bar"></div>
+                        </div>
+                    </div>
+                    <!-- Mobile Skill Bar End -->
+                    <div class="do-skillbar clearfix" data-percent="85">
+                        <p class="do-skillbar-title">Mobile Technology<span class="skill-bar-percent"></span></p>
+
+                        <div class="do-skillbar-bar-wrapper">
+                            <div class="do-skillbar-bar"></div>
+                        </div>
+                    </div>
+
+                    <!-- Support -->
+
+                    <div class="do-skillbar clearfix" data-percent="100">
+                        <p class="do-skillbar-title">Client Support<span class="skill-bar-percent"></span></p>
+
+                        <div class="do-skillbar-bar-wrapper">
+                            <div class="do-skillbar-bar"></div>
+                        </div>
+                    </div>
+                </div>
+                <!-- SKILLBAR END -->
             </div>
         </div>
-    </section>
-    <!-- do Main Slider End -->
+    </div>
+</section>
+<!-- ABOUT US DETAILS SECTION END -->
 
-    <!--================================
-        ABOUT US DETAILS SECTION 
-    =================================-->
-    <section class="do-about-us-section">
-        <div class="container">
-            <div class="row">
+<!--================================
+    CUSTOM SECTION
+=================================-->
+<section class="do-about-custom-section">
+    <div class="container">
+        <div class="row">
+            <!-- CUSTOM SECTION IMG -->
+            <div class="do-about-custom-img col-md-6 col-sm-6 col-xs-12 appear fadeInUp" data-wow-duration="1.5s"
+                 data-wow-delay=".4s">
+                <img src="images/custom-img-2.png" alt="">
+            </div>
+            <!-- CUSTOM SECTION IMG END -->
+
+            <!-- CUSTOM SECTION DETAILS -->
+            <div class="do-about-custom-details col-md-6 col-sm-6 col-xs-12">
                 <!-- SECTION SUBHEADING -->
-                <h2 class="do-section-subheading col-md-12 col-sm-12 col-xs-12">we are expert</h2>
+                <h2 class="do-section-subheading">We love What We do</h2>
                 <!-- SECTION SUBHEADING END -->
 
-                <div class="do-about-us-details">
-                    <!-- TEXT ROW 1 -->
-                    <div class="col-md-4 col-sm-4 col-xs-12 do-about-text-row">
-                        <p>
-                            No one rejects, dislikes, or avoids pleasure itself, because is counter buti because those who do not know how an puIse pleasure rationally encounter consequences that are painci. Nor again is there anyone who loves or pursues and desirn. No one rejects, dislikes, or avoids pleasure itself, because is counter buti because those who do not know how an puIse. No one rejects, dislikes, or avoids pleasure itself, because is pleasure rationally encounter consequences that are painci counter buti because those who do not know how an puIse
-                        </p>
-                    </div>
-                    <!-- TEXT ROW 1 END -->
+                <p>No one rejects, dislikes, or avoids pleasure itself, because is counter because upon those who do not
+                    know how an puIse pleasure rationally encounter consequnce is Nor again is there anyone who
+                    loves</p>
 
-                    <!-- TEXT ROW 2 -->
-                    <div class="col-md-4 col-sm-4 col-xs-12 do-about-text-row">
-                        <p>
-                            counter buti because those who do not know how an puIse pleasure rationally encounter consequences that are painci. No one rejects, dislikes, or avoids pleasure itself, because is counter buti because those who do not know how an puIse pleasure rationally encounter consequences that are painci. Nor again is there anyone who loves or pursues and desirn
-                        </p>
-                        <p>pleasure rationally encounter consequences that are painci counter buti because those who do.</p>
-                    </div>
-                    <!-- TEXT ROW 2 END -->
+                <ul class="do-icon-list">
+                    <li><i class="fa fa-angle-right"></i> Fresh clean theme for business or personal</li>
+                    <li><i class="fa fa-angle-right"></i> Made with <span class="do-color-text">love</span> and care for
+                        you
+                    </li>
+                    <li><i class="fa fa-angle-right"></i> Pixel perfect coding done</li>
+                </ul>
 
-                    <!-- SKILLBAR -->
-                    <div class="col-md-4 col-sm-4 col-xs-12 do-our-skill">
-
-                        <!-- Design Skill Bar -->
-                        <div class="do-skillbar clearfix" data-percent="70">
-                            <p class="do-skillbar-title">Design<span class="skill-bar-percent"></span></p>
-                            <div class="do-skillbar-bar-wrapper">
-                                <div class="do-skillbar-bar"></div>
-                            </div>
-                        </div> 
-                        <!-- Design Skill Bar End -->
-
-                        <!-- Code Skill Bar -->
-                        <div class="do-skillbar clearfix" data-percent="80">
-                            <p class="do-skillbar-title">code<span class="skill-bar-percent"></span></p>
-                            <div class="do-skillbar-bar-wrapper">
-                                <div class="do-skillbar-bar"></div>
-                            </div>
-                        </div>
-                        <!-- Code Skill Bar End -->
-
-                        <!-- Writing Skill Bar -->
-                        <div class="do-skillbar clearfix" data-percent="80">
-                            <p class="do-skillbar-title">writing<span class="skill-bar-percent"></span></p>
-                            <div class="do-skillbar-bar-wrapper">
-                                <div class="do-skillbar-bar"></div>
-                            </div>
-                        </div>
-                        <!-- Writing Skill Bar End -->
-
-                        <!-- Photography Skill Bar -->
-                        <div class="do-skillbar clearfix" data-percent="40">
-                            <p class="do-skillbar-title">photography<span class="skill-bar-percent"></span></p>
-                            <div class="do-skillbar-bar-wrapper">
-                                <div class="do-skillbar-bar"></div>
-                            </div>
-                        </div>
-                        <!-- Photography Skill Bar End -->
-
-                    </div>
-                    <!-- SKILLBAR END -->
-                </div>
+                <a href="about_us.html#" class="do-btn-round-solid">more</a>
             </div>
+            <!-- CUSTOM SECTION DETAILS END -->
         </div>
-    </section>
-    <!-- ABOUT US DETAILS SECTION END -->
-
-    <!--================================
-        CUSTOM SECTION 
-    =================================-->
-    <section class="do-about-custom-section">
-        <div class="container">
-            <div class="row">
-                <!-- CUSTOM SECTION IMG -->
-                <div class="do-about-custom-img col-md-6 col-sm-6 col-xs-12 appear fadeInUp" data-wow-duration="1.5s" data-wow-delay=".4s">
-                    <img src="images/custom-img-2.png" alt="">
-                </div>
-                <!-- CUSTOM SECTION IMG END -->
-
-                <!-- CUSTOM SECTION DETAILS -->
-                <div class="do-about-custom-details col-md-6 col-sm-6 col-xs-12">
-                    <!-- SECTION SUBHEADING -->
-                    <h2 class="do-section-subheading">love design</h2>
-                    <!-- SECTION SUBHEADING END -->
-                    
-                    <p>No one rejects, dislikes, or avoids pleasure itself, because is counter because upon those who do not know how an puIse pleasure rationally encounter consequnce is Nor again is there anyone who loves</p>
-
-                    <ul class="do-icon-list">
-                        <li><i class="fa fa-angle-right"></i> Fresh clean theme for business or personal</li>
-                        <li><i class="fa fa-angle-right"></i> Made with <span class="do-color-text">love</span> and care for you</li>
-                        <li><i class="fa fa-angle-right"></i> Pixel perfect coding done</li>
-                    </ul>
-
-                    <a href="about_us.html#" class="do-btn-round-solid">more</a>
-                </div>
-                <!-- CUSTOM SECTION DETAILS END -->
-            </div>
-        </div>
-    </section>
-    <!-- CUSTOM SECTION END -->
+    </div>
+</section>
+<!-- CUSTOM SECTION END -->
 
 <?php
 require_once('footer.php');
 ?>
-              					
+
 </body>
 </html>
